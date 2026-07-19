d4fda43a52cca49865d7cc33e6406590d6e6ab3e
Pratik-Khomane <pratikkhomane6@gmail.com>
2026-07-19 20:48:44 +0530

    chore: initialize Spring Boot authentication backend

---
+++ auth-app-bakend/src/main/java/com/auth_app/auth_app_bakend/AuthAppBakendApplication.java
@@ -0,0 +1,13 @@
+package com.auth_app.auth_app_bakend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAppBakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAppBakendApplication.class, args);
+	}
+
+}
