c9466aae444d4eb8ec22c2b44babb32352135576
Pratik-Khomane <pratikkhomane6@gmail.com>
2026-07-21 14:49:03 +0530

    feat: configure Spring Security authentication

---
+++ auth-app-bakend/src/main/java/com/auth_app/auth_app_bakend/security/CustomUserDetailService.java
@@ -0,0 +1,23 @@
+package com.auth_app.auth_app_bakend.security;
+
+import com.auth_app.auth_app_bakend.exceptions.ResourceNotFoundException;
+import com.auth_app.auth_app_bakend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("Invalid Username or Password"));
+    }
+}
